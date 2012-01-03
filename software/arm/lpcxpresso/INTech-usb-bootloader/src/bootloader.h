8c655cc613ad3402dd16c8cb3593d8d6ae21d236
Jérémy <jeremy@minet.net>
2012-01-03 12:18:34 +0100

    Rajout du bootloader USB.
    Attention : il ne fonctionne qu'avec windows (pour l'instant).

---
+++ software/arm/lpcxpresso/INTech-usb-bootloader/src/bootloader.h
@@ -0,0 +1,10 @@
+
+//#include <cr_section_macros.h>
+
+/*place buffer in section .ram2*/
+static unsigned char boot_buffer[5] __attribute__ ((section (".ram2")))={0};
+
+//static unsigned char boot_flag01;
+//static unsigned char boot_flag02;
+//static unsigned char boot_flag03;
+//static unsigned char boot_flag04;
