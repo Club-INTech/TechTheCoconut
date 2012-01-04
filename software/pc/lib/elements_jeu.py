56bca96223903a7ec1d54984f77ceecfcabed6f9
Thibaut REMY <thibaut.remy@telecom-sudparis.eu>
2012-01-04 18:51:04 +0100

    Ajout d'attribut aux éléments de jeu

--- software/pc/lib/elements_jeu.py
+++ software/pc/lib/elements_jeu.py
@@ -4,29 +4,53 @@ class Disque:
     """
     Classe de créer l'élément de jeu disque
     """
-    def __init__(self):
-        pass
+    def __init__(self, position):
+      """
+      :param position: Position du disque
+      :type position: Point
+      """
+      self.position = position # position est de type Point
+      self.rayon = 5 #TODO changer la valeur numérique
 
 class Lingot:
     """
     Classe de créer l'élément de jeu lingot
     """
-    def __init__(self):
-        pass
-
+    def __init__(self, position, orientation):
+      """
+      :param position: Position du disque
+      :type position: Point
+      """
+        self.position = position
+        self.orientation = orientation  # Angle que fait le lingot avec le bas de la table, en radians
+        
 class Totem:
     """
     Classe de créer l'élément de jeu totem
     """
-    def __init__(self):
-        pass
+    def __init__(self, position):
+      """
+      :param position: Position du disque
+      :type position: Point
+      """
+        self.position = position
+        self.couleur = "ROUGE" # cet attribut correspond au joueur à qui appartient le todo
+        #			 TODO gérer l'assignation de cet attibut
+        self.longueur = 5 #TODO changer la valeur numérique
+        self.largeur = self.longueur #cette variable est inutile mais au moins on peut l'utiliser
+        
+        self.hauteur = 5 #TODO changer la valeur numérique
 
 class Poussoir:
     """
     Classe de créer l'élément de jeu poussoir
     """
-    def __init__(self):
-        pass
+    def __init__(self, position):
+      """
+      :param position: Position du disque
+      :type position: Point
+      """
+        self.position = position
 
 class Carte_tresor:
     """
@@ -43,3 +67,22 @@ class Zone:
     """
     def __init__(self):
         pass
+      
+class Point :
+  """
+  Classe pour la position des éléments  
+  """
+  def __int__(self, positionX, positionY):
+    """
+    :param positionX: abscisse du point, en mm
+    :type positionY: float
+    
+    :param positionY: ordonnée du point, en mm
+    :type positionY: float
+    """
+    self.x = positionX
+    self.y = positionY
+    
+  def distance(self, point2) :
+    from math import sqrt
+    return sqrt((point2.x - self.x)*(point2.x - self.x) + (point2.y - self.y)*(point2.y - self.y))
