9b897948b96a07b250c2761493b4c2000f3daa51
Pierre de Beaucorps <pierre@debeaucorps.com>
2012-01-17 16:18:07 +0100

    fonctions de suppressions des noeuds en collision avec un objet (version perdue au dernier push)

--- software/pc/lib/recherche_chemin/graph_tool_test.py
+++ software/pc/lib/recherche_chemin/graph_tool_test.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 
+import os
+import sys
 from graph_tool.all import *
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../outils_math"))
+from collisionRectangles import Rectangle,collision
+
 
 g = Graph(directed=False)
 posX = g.new_vertex_property("int")
@@ -9,80 +14,143 @@ poids = g.new_edge_property("double")
 poidsDirect = 1.
 poidsDiag = 1.41
 pas = 10 # pas en mm
-largeur = int(20.001/pas)#TODO largeur = int(constantes["Coconut"]["largeur"]/pas)
-longueur = int(54./pas)#TODO longueur = int(constantes["Coconut"]["longueur"]/pas)
+largeur = int(40.001/pas)#TODO largeur = int(constantes["Coconut"]["largeur"]/pas)
+longueur = int(64./pas)#TODO longueur = int(constantes["Coconut"]["longueur"]/pas)
 #décalage des axes
 axeX=0
 axeY=0
 
+objet=Rectangle(20.,15.,-0.7853,10.,20.)
 
-#génération des noeuds, avec positions
-#et des arêtes, avec poids
-
-#premier noeud
-g.add_vertex()
-posX[g.vertex(0)] = 0+axeX
-posY[g.vertex(0)] = 0+axeY
-#première ligne
-for j in range(1,longueur):
-  g.add_vertex()
-  posX[g.vertex(longueur*0+j)] = j*pas+axeX
-  posY[g.vertex(longueur*0+j)] = 0*pas+axeY
-  #noeud à gauche
-  g.add_edge(g.vertex(longueur*0+j-1),g.vertex(longueur*0+j))
-  poids[g.edge(g.vertex(longueur*0+j-1),g.vertex(longueur*0+j))]=poidsDirect
-#autres lignes
-for i in range(1,largeur):
-  #premier de la ligne
-  g.add_vertex()
-  posX[g.vertex(longueur*i)] = 0+axeX
-  posY[g.vertex(longueur*i)] = i*pas+axeY
-  #2 noeuds dessus
-  g.add_edge(g.vertex(longueur*(i-1)),g.vertex(longueur*i))
-  poids[g.edge(g.vertex(longueur*(i-1)),g.vertex(longueur*i))]=poidsDirect
-  g.add_edge(g.vertex(longueur*(i-1)+1),g.vertex(longueur*i))
-  poids[g.edge(g.vertex(longueur*(i-1)+1),g.vertex(longueur*i))]=poidsDiag
-  #corps de la ligne
-  for j in range(1,longueur-1):
-      g.add_vertex()
-      posX[g.vertex(longueur*i+j)] = j*pas+axeX
-      posY[g.vertex(longueur*i+j)] = i*pas+axeY
-      #noeud à gauche
-      g.add_edge(g.vertex(longueur*i+j-1),g.vertex(longueur*i+j))
-      poids[g.edge(g.vertex(longueur*i+j-1),g.vertex(longueur*i+j))]=poidsDirect
-      #3 noeuds dessus
-      g.add_edge(g.vertex(longueur*(i-1)+j-1),g.vertex(longueur*i+j))
-      poids[g.edge(g.vertex(longueur*(i-1)+j-1),g.vertex(longueur*i+j))]=poidsDiag
-      g.add_edge(g.vertex(longueur*(i-1)+j),g.vertex(longueur*i+j))
-      poids[g.edge(g.vertex(longueur*(i-1)+j),g.vertex(longueur*i+j))]=poidsDirect
-      g.add_edge(g.vertex(longueur*(i-1)+j+1),g.vertex(longueur*i+j))
-      poids[g.edge(g.vertex(longueur*(i-1)+j+1),g.vertex(longueur*i+j))]=poidsDiag
-  #dernier de la ligne
-  g.add_vertex()
-  posX[g.vertex(longueur*(i+1)-1)] = (longueur-1)*pas+axeX
-  posY[g.vertex(longueur*(i+1)-1)] = i*pas+axeY
-  #noeud à gauche
-  g.add_edge(g.vertex(longueur*(i+1)-2),g.vertex(longueur*(i+1)-1))
-  poids[g.edge(g.vertex(longueur*(i+1)-2),g.vertex(longueur*(i+1)-1))]=poidsDirect
-  #2 noeuds dessus
-  g.add_edge(g.vertex(longueur*i-1),g.vertex(longueur*(i+1)-1))
-  poids[g.edge(g.vertex(longueur*i-1),g.vertex(longueur*(i+1)-1))]=poidsDirect
-  g.add_edge(g.vertex(longueur*i-2),g.vertex(longueur*(i+1)-1))
-  poids[g.edge(g.vertex(longueur*i-2),g.vertex(longueur*(i+1)-1))]=poidsDiag
+def rechercheChemin(debut,fin):
+    """
+    fonction de recherche de chemin, utilisant le meilleur algorithme codé
+    """
+    return AStar(debut,fin)
+        
+def AStar(debut,fin):
+    """
+    algorithme A*, sur une table de jeu discrétisée "par cases"
+    """
+    
+    discretiseTable()
+    
+    n=selectNoeudProche(objet.x,objet.y)
+    
+    #case=Rectangle(posX[n],posY[n],0,pas,pas)
+    #print collision(case,objet)
+    
+    l=listerNoeuds(objet,[n],[n])
+    print l
+    supprimerNoeuds(l)
     
     
-for i in range(1,largeur):
-    for j in range(longueur):
-      print str(longueur*i+j) + " :\t" + str(posX[g.vertex(longueur*i+j)]) + ',\t' + str(posY[g.vertex(longueur*i+j)]) + " :\t" + str(poids[g.edge(g.vertex(longueur*i+j),g.vertex(longueur*(i-1)+j))])
-      
-      
-#TODO : suppression des noeuds inaccessibles (triangulation de Delaunay)
+    #TODO : suppression des noeuds inaccessibles (triangulation de Delaunay)
+    #TODO : algorithme A* sur le graphe obtenu
+    #TODO : retour de la structure cheminObtenu
+    graph_draw(g, vprops={"label": g.vertex_index}, output="map_suppr.pdf", splines='false',vsize=0.11, elen=1)
+    
+    
+    
+def supprimerNoeuds(listeNoeuds):
+    for n in listeNoeuds:
+        g.remove_vertex(n)
+    
+def NoeudsVoisins(noeud,listeVoisins):
+    nouveaux=[]
+    for v in noeud.out_neighbours():
+        if not (v in listeVoisins):
+            nouveaux.append(v)
+    return nouveaux   
+    
+def listerNoeuds(objet,listeVoisins,aParcourir):
+    while aParcourir!=[]:
+        noeud=aParcourir.pop()
+        case=Rectangle(posX[noeud],posY[noeud],0,pas,pas)
+        if collision(case,objet):
+            listeVoisins.extend(NoeudsVoisins(noeud,listeVoisins))
+            aParcourir.extend(NoeudsVoisins(noeud,listeVoisins))
+    return listeVoisins
+        
+def selectNoeudProche(x,y):
+    """
+    à partir des coordonnée d'un objet inaccessible, renvoi un noeud du graphe contenant ces coordonnées.
+    ce sera un premier noeud eliminé, et des tests de collision seront effectués par récurrence sur ses voisins.
+    """
+    for v in g.vertices() :
+        if (abs(posX[v]-x) <= pas/2) and (abs(posY[v]-y) <= pas/2) :
+            return v
+            break
+            
+    
+def discretiseTable():    
+    """
+    génération des noeuds, avec positions
+    et des arêtes, avec poids
+    """
+    #premier noeud
+    g.add_vertex()
+    posX[g.vertex(0)] = 0+axeX
+    posY[g.vertex(0)] = 0+axeY
+    #première ligne
+    for j in range(1,longueur):
+        g.add_vertex()
+        posX[g.vertex(longueur*0+j)] = j*pas+axeX
+        posY[g.vertex(longueur*0+j)] = 0*pas+axeY
+        #noeud à gauche
+        g.add_edge(g.vertex(longueur*0+j-1),g.vertex(longueur*0+j))
+        poids[g.edge(g.vertex(longueur*0+j-1),g.vertex(longueur*0+j))]=poidsDirect
+    #autres lignes
+    for i in range(1,largeur):
+        #premier de la ligne
+        g.add_vertex()
+        posX[g.vertex(longueur*i)] = 0+axeX
+        posY[g.vertex(longueur*i)] = i*pas+axeY
+        #2 noeuds dessus
+        g.add_edge(g.vertex(longueur*(i-1)),g.vertex(longueur*i))
+        poids[g.edge(g.vertex(longueur*(i-1)),g.vertex(longueur*i))]=poidsDirect
+        g.add_edge(g.vertex(longueur*(i-1)+1),g.vertex(longueur*i))
+        poids[g.edge(g.vertex(longueur*(i-1)+1),g.vertex(longueur*i))]=poidsDiag
+        #corps de la ligne
+        for j in range(1,longueur-1):
+            g.add_vertex()
+            posX[g.vertex(longueur*i+j)] = j*pas+axeX
+            posY[g.vertex(longueur*i+j)] = i*pas+axeY
+            #noeud à gauche
+            g.add_edge(g.vertex(longueur*i+j-1),g.vertex(longueur*i+j))
+            poids[g.edge(g.vertex(longueur*i+j-1),g.vertex(longueur*i+j))]=poidsDirect
+            #3 noeuds dessus
+            g.add_edge(g.vertex(longueur*(i-1)+j-1),g.vertex(longueur*i+j))
+            poids[g.edge(g.vertex(longueur*(i-1)+j-1),g.vertex(longueur*i+j))]=poidsDiag
+            g.add_edge(g.vertex(longueur*(i-1)+j),g.vertex(longueur*i+j))
+            poids[g.edge(g.vertex(longueur*(i-1)+j),g.vertex(longueur*i+j))]=poidsDirect
+            g.add_edge(g.vertex(longueur*(i-1)+j+1),g.vertex(longueur*i+j))
+            poids[g.edge(g.vertex(longueur*(i-1)+j+1),g.vertex(longueur*i+j))]=poidsDiag
+        #dernier de la ligne
+        g.add_vertex()
+        posX[g.vertex(longueur*(i+1)-1)] = (longueur-1)*pas+axeX
+        posY[g.vertex(longueur*(i+1)-1)] = i*pas+axeY
+        #noeud à gauche
+        g.add_edge(g.vertex(longueur*(i+1)-2),g.vertex(longueur*(i+1)-1))
+        poids[g.edge(g.vertex(longueur*(i+1)-2),g.vertex(longueur*(i+1)-1))]=poidsDirect
+        #2 noeuds dessus
+        g.add_edge(g.vertex(longueur*i-1),g.vertex(longueur*(i+1)-1))
+        poids[g.edge(g.vertex(longueur*i-1),g.vertex(longueur*(i+1)-1))]=poidsDirect
+        g.add_edge(g.vertex(longueur*i-2),g.vertex(longueur*(i+1)-1))
+        poids[g.edge(g.vertex(longueur*i-2),g.vertex(longueur*(i+1)-1))]=poidsDiag
+        
+    """
+    trace
+    for i in range(1,largeur):
+        for j in range(longueur):
+            print str(longueur*i+j) + " :\t" + str(posX[g.vertex(longueur*i+j)]) + ',\t' + str(posY[g.vertex(longueur*i+j)]) + " :\t" + str(poids[g.edge(g.vertex(longueur*i+j),g.vertex(longueur*(i-1)+j))])
+    """
+    
+    #g.remove_vertex(g.vertex(longueur*(largeur/2)-(longueur/2)))
+    
+    
 
-
+rechercheChemin(0,0)
+      
 #g, pos = gt.triangulation(points, type="delaunay")
-
-#for e in g.edges():
- # touch_v = g.new_vertex_property("bool")
- # touch_e = g.new_edge_property("bool")
- # target = g.vertex(1)
- # dist, pred = gt.astar_search(g, g.vertex(0), weight, VisitorExample(touch_v, touch_e, target), heuristic=lambda v: h(v, target, pos))
+#dist, pred = gt.astar_search(g, g.vertex(0), weight, VisitorExample(touch_v, touch_e, target), heuristic=lambda v: h(v, target, pos))
