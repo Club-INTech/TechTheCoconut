59b25aae3b5f197e0a08d91a3cedc241f9218f2c
Anthony VEREZ <netantho@gmail.com>
2012-01-29 02:45:49 +0100

    PC : lib.serie et lib.serie_simple finis avec tests et doc

--- software/pc/lib/serie.py
+++ software/pc/lib/serie.py
@@ -3,18 +3,17 @@
 import serial
 import threading
 import Queue
-import time
 
 import log
 log = log.Log()
 
-class SerieThread(threading.Thread, serial.Serial):
+class Serie(threading.Thread, serial.Serial):
     """
     Classe de créer une liaison Série utilisant un thread (ie non bloquante)\n
     \n
     Pour la démarrer utiliser la méthode start()\n
     Les messages sont stockés dans l'attribut d'instance file_attente qui est un objet LifoQueue http://www.doughellmann.com/PyMOTW/Queue/index.html#lifo-queue \n
-    Pour l'arrêter utiliser l'instruction del (del mavariable)\n
+    Pour l'arrêter utiliser la méthode stop()\n
     \n
     :param peripherique: chemin du périphérique utilisant la liaison série
     :type peripherique: string
@@ -30,17 +29,23 @@ class SerieThread(threading.Thread, serial.Serial):
     """
     def __init__(self, peripherique, nom, debit, timeout, parite=None):
         self.peripherique = peripherique
+        self.nom = nom
         # File d'attente LIFO des messages venant de cette liaison
         self.file_attente = Queue.LifoQueue()
-        log.logger.info("Initialisation de la liaison série threadée sur "+peripherique+" avec un débit de baud de "+str(debit)+" et un timeout de "+str(timeout))
+        log.logger.info("Initialisation de la liaison série threadée "+nom+" sur "+peripherique+" avec un débit de baud de "+str(debit)+" et un timeout de "+str(timeout))
+        if parite != None:
+            exec("parite = serial."+parite)
         try:
             threading.Thread.__init__(self, name=nom, target=self.lire)
+            self.active = True
             if parite == None:
                 serial.Serial.__init__(self, peripherique, debit, timeout=timeout)
             else:
                 serial.Serial.__init__(self, peripherique, debit, target=self.lire, timeout=timeout, parity=parite)
         except:
-            log.logger.error("Erreur d'initialisation de la liaison série threadée sur "+peripherique+" avec un débit de baud de "+str(debit)+" et un timeout de "+str(timeout))
+            self.active = False
+            log.logger.error("Erreur d'initialisation de la liaison série threadée "+nom+" sur "+peripherique+" avec un débit de baud de "+str(debit)+" et un timeout de "+str(timeout))
+            self.stop()
 
     def ecrire(self, msg):
         """
@@ -51,7 +56,7 @@ class SerieThread(threading.Thread, serial.Serial):
         :return: Nombre de caractères envoyés
         :rtype: int
         """
-        log.logger.debug("Écrire sur la liaison série "+self.peripherique+" : "+msg)
+        log.logger.debug("Écrire sur la liaison série "+self.nom+" : "+msg)
         return self.write(msg+"\r\n")
     
     def lire(self):
@@ -60,71 +65,22 @@ class SerieThread(threading.Thread, serial.Serial):
         
         :return: Chaîne lue
         :rtype: string
-        :TODO; permettre d'utiliser correction self.join()
-        :TODO: trouver solution pour les logs
         """
-        while 42:
+        while self.active:
             lu = self.readline()
             lu = lu.split("\r\n")[0]
             if lu != '':
-                #log.logger.debug("Lecture sur la liaison série "+self.peripherique+" : "+lu)
+                log.logger.debug("Lecture sur la liaison série "+self.nom+" : "+lu)
                 self.file_attente.put(lu)
-
-    def __del__(self):
-        """
-        Destructeur qui ferme proprement la liaison série
-        """
-        log.logger.info("Suppression de la liaison série sur "+self.peripherique)
-        self.join()
-        self.close()
-
-class SerieSimple(serial.Serial):
-    """
-    Classe de créer une liaison Série sans Thread (ie bloquante)
     
-    :param peripherique: chemin du périphérique utilisant la liaison série
-    :type peripherique: string
-    :param debit: Débit de baud de la liaison
-    :type debit: int
-    :param timeout: Timeout de la liaison en secondes
-    :type timeout: int
-    :TODO: Mettre le débit de Baud par défaut
-    """
-    def __init__(self, peripherique, debit, timeout):
-        self.peripherique = peripherique
-        log.logger.info("Initialisation de la liaison série simple sur "+peripherique+" avec un débit de baud de "+str(debit)+" et un timeout de "+str(timeout))
-        try:
-            serial.Serial.__init__(self, peripherique, debit, timeout=timeout)
-        except:
-            log.logger.error("Erreur d'initialisation de la liaison série simple sur "+peripherique+" avec un débit de baud de "+str(debit)+" et un timeout de "+str(timeout))
-
-    def ecrire(self, msg):
+    def stop(self):
         """
-        Écrire une information vers un périphérique puis retourner à la ligne
-        
-        :param msg: message à donner au périphérique
-        :type msg: string
-        :return: Nombre de caractères envoyés
-        :rtype: int
+        Arrête la liaison série
         """
-        log.logger.debug("Écrire sur la liaison série "+self.peripherique+" : "+msg)
-        return self.write(msg+"\r\n")
-    
-    def lire(self):
-        """
-        Lire une information venant d'un périphérique jusqu'au retour à la ligne
-        
-        :return: Chaîne lue
-        :rtype: string
-        """
-        lu = self.readline()
-        lu = lu.split("\r\n")[0]
-        log.logger.debug("Lecture sur la liaison série "+self.peripherique+" : "+lu)
-        return lu;
-
-    def __del__(self):
-        """
-        Destructeur qui ferme proprement la liaison série
-        """
-        log.logger.info("Suppression de la liaison série sur "+self.peripherique)
-        self.close()
+        log.logger.info("Suppression de la liaison série "+self.nom+" sur "+self.peripherique)
+        self.active = False
+        if self.active:
+            self.join()
+        # Pour gérer une mauvaise parité qui ne lance par la liaison
+        if hasattr(self, "_isOpen"):
+            self.close()
