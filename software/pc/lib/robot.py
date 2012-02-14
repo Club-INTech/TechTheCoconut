ec9c0cb10b5539df71f31ecc2a908f495e197a31
François Monniot <francoismonniot@gmail.com>
2012-02-14 20:42:03 +0100

    Ajout d'attribut au robot pour pouvoir tester la visualisation Seul les attributs devront etre conservé (compatibilité visu)

--- software/pc/lib/robot.py
+++ software/pc/lib/robot.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
 
 class Robot:
+    
+    rayon = 25 
     """
-    Classe qui gère le robot
+    Classe permettant de gérer le robot
+    :TODO: Refaire  l'initialisation du robot; Basiquement la déclaration de position et d'orientation pourrait etre fait ailleurs (idée a développer)
+    
+    :param position: Position du robot
+    :type position: Point
+    
+    :param orientation: Orientation du robot
+    :type orientation: float
     """
-    def __init__(self):
-        pass
+    def __init__(self, position, orientation):
+        self.position = position
+        self.orientation = orientation
