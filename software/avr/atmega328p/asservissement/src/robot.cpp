b6a197728b52b113fd94f8266e927b85422d42f1
Pierre de Beaucorps <pierre@debeaucorps.com>
2012-02-18 04:29:51 +0100

    AVR : passage des last_dist et last_angle en attributs de classe pour détecter que le déplacement est terminé dans translater() et tourner()

--- software/avr/atmega328p/asservissement/src/robot.cpp
+++ software/avr/atmega328p/asservissement/src/robot.cpp
@@ -11,16 +11,10 @@ Robot::Robot() : couleur_('r')
 				,y_(0)
 				,translation(1,1,0)
 				,rotation(1.5,1,0)
-				,last_angle_rad_(0.)
-				,last_dist_mm_(0.)
+				,last_angle_tic_(0)
+				,last_dist_tic_(0)
 
 {
-	
-	if(couleur_ == 'r')
-		last_angle_rad_ = 0.;
-	else
-		//angle de Pi pour le robot violet
-		last_angle_rad_ =  3.141592654;
 	
 	TWI_init();
 	Serial<0>::init();
@@ -31,31 +25,37 @@ Robot::Robot() : couleur_('r')
 void Robot::asservir(int32_t distance, int32_t angle)
 {
 	int32_t pwmTranslation = translation.pwm(distance);
-// 	int32_t pwmTranslation = 0;
 	int32_t pwmRotation = rotation.pwm(angle);
-// 	int32_t pwmRotation = 0;
-	Serial<0>::print(pwmTranslation);
  	moteurDroit.envoyerPwm(pwmTranslation + pwmRotation);
  	moteurGauche.envoyerPwm(pwmTranslation - pwmRotation);
 }
 
+
 void Robot::updatePosition(int32_t distance_tic, int32_t angle_tic)
 {
     
-	static int32_t last_distance_tic = 0;
-	static int32_t last_angle_tic = 0;
 
 	static const float CONVERSION_TIC_MM = 1.04195690364;
 	static const float CONVERSION_TIC_RADIAN = 0.000737463064;
 	
-	float delta_angle_rad = (angle_tic - last_angle_tic)* CONVERSION_TIC_RADIAN;
-	float delta_distance_mm = (distance_tic - last_distance_tic) * CONVERSION_TIC_MM;
+	
+	float delta_angle_rad = (angle_tic - last_angle_tic_)* CONVERSION_TIC_RADIAN;
+	float delta_distance_mm = (distance_tic - last_dist_tic_) * CONVERSION_TIC_MM;
+	
     
-
+	
 	if(delta_angle_rad==0)
 	{
-		x_ += ( delta_distance_mm * cos( last_angle_rad_ ) );
-		y_ += ( delta_distance_mm * sin( last_angle_rad_ ) );
+		if(couleur_ == 'v'){
+			//angle de Pi pour le robot violet (en tic)
+			float last_angle_radian = last_angle_tic_ * CONVERSION_TIC_RADIAN;
+			x_ += ( delta_distance_mm * cos( last_angle_radian ) );
+			y_ += ( delta_distance_mm * sin( last_angle_radian ) );
+		}else{
+			float last_angle_radian = (last_angle_tic_ + 4260)* CONVERSION_TIC_RADIAN;
+			x_ += ( delta_distance_mm * cos( last_angle_radian ) );
+			y_ += ( delta_distance_mm * sin( last_angle_radian ) );
+		}
 	
 	}
 	else
@@ -63,19 +63,26 @@ void Robot::updatePosition(int32_t distance_tic, int32_t angle_tic)
 		float r = delta_distance_mm/delta_angle_rad;
 		float angle_radian =  angle_tic * CONVERSION_TIC_RADIAN;
 	
-		x_ += r * (-sin(angle_radian) + sin(last_angle_rad_));
-		y_ += r * (cos(angle_radian) - cos(last_angle_rad_));
+		
+		if(couleur_ == 'v'){
+			//angle de Pi pour le robot violet (en tic)
+			float last_angle_radian = last_angle_tic_ * CONVERSION_TIC_RADIAN;
+			x_ += r * (-sin(angle_radian) + sin(last_angle_radian));
+			y_ += r * (cos(angle_radian) - cos(last_angle_radian));
+		}else{
+			float last_angle_radian = (last_angle_tic_ + 4260)* CONVERSION_TIC_RADIAN;
+			x_ += r * (-sin(angle_radian) + sin(last_angle_radian));
+			y_ += r * (cos(angle_radian) - cos(last_angle_radian));
+		}
+		
+		
 	}
 	
-	//met à jour la distance parcourue (mm) sur le dernier segment
-	last_dist_mm_ += (distance_tic - last_distance_tic) * CONVERSION_TIC_MM;
+	//met à jour la distance parcourue
+	last_dist_tic_ = distance_tic;
 	
-	last_distance_tic = distance_tic;
-	
-	//met à jour l'orientation (rad) connue du robot
-	last_angle_rad_ +=  (angle_tic - last_angle_tic) * CONVERSION_TIC_RADIAN;
-	
-	last_angle_tic = angle_tic;
+	//met à jour l'orientation du robot
+	last_angle_tic_ = angle_tic;
 	
 }
 
@@ -147,41 +154,35 @@ return (int16_t)y_;
 }
 
 
-bool Robot::gotoPos(int16_t x, int16_t y)
+void Robot::gotoPos(int16_t x, int16_t y)
 {
-	//pourquoi _x et _y sont en float au fait ?
+	static const float CONVERSION_TIC_RADIAN = 0.000737463064;
+	static const float CONVERSION_TIC_MM = 1.04195690364;
+	
 	float delta_x = (x-x_);
 	float delta_y = (y-y_);
-	if(tourner((uint16_t)(atan(delta_y/delta_x)-last_angle_rad_)))
-	{
-		if(translater((uint16_t)sqrt(delta_x*delta_x+delta_y*delta_y)))
-			return true;
-		else
-			return false;
-	}else
-		return false;
+	
+	if(couleur_=='v')
+		tourner(atan2(delta_y,delta_x)/CONVERSION_TIC_RADIAN - 4260);
+	else
+		tourner(atan2(delta_y,delta_x)/CONVERSION_TIC_RADIAN);
+	translater(sqrt(delta_x*delta_x+delta_y*delta_y)/CONVERSION_TIC_MM);
 }
 
-bool Robot::translater(uint16_t distance)
+void Robot::translater(uint16_t distance)
 {
-	static float eps = 0.5;//incertitude
-	last_dist_mm_ = 0.;
-	translation.consigne((int32_t)distance);
-	while(abs(last_dist_mm_ - distance) > eps);
+	uint32_t cons_dist_tic = translation.consigne();
+	translation.consigne((int32_t)(cons_dist_tic+distance));
+	while(last_dist_tic_ != cons_dist_tic+distance);
 		//attend d'atteindre la consigne
 		//on peut rajouter un timeout pour détecter un problème
-	
-	return true;
 }
 
-bool Robot::tourner(uint16_t angle)
+void Robot::tourner(int16_t angle)
 {
-	static float eps = 0.001;//incertitude
 	rotation.consigne((int32_t)angle);
 	
-	while(abs(last_angle_rad_ - angle) > eps);
+	while(last_angle_tic_ != angle);
 		//attend d'atteindre la consigne
 		//on peut rajouter un timeout pour détecter un problème
-	
-	return true;
 }
