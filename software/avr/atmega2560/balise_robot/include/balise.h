d2a84c7e3bfb47fde72a87dc57da8b745ba9120f
Philippe <Phil.Tillet@gmail.com>
2012-02-23 23:58:36 +0100

    Correction de l'intégrité du compte tour

--- software/avr/atmega2560/balise_robot/include/balise.h
+++ software/avr/atmega2560/balise_robot/include/balise.h
@@ -17,6 +17,7 @@ class Balise : public Singleton<Balise>{
 	private:
 		volatile uint16_t max_counter_;
 		volatile uint16_t toptour_;
+		bool is_toptour_available_;
 /*
 		typedef Timer<2,ModeFastPwm,1> T_2;
 		
