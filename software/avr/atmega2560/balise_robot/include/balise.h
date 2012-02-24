53f6df904803508c1216d3f91845d1f4c8bac1b2
Paul <paul@paul-HP-Pavilion-dv6500-Notebook-PC>
2012-02-24 14:02:06 +0100

    Suppression des dernières modifs

--- software/avr/atmega2560/balise_robot/include/balise.h
+++ software/avr/atmega2560/balise_robot/include/balise.h
@@ -17,7 +17,6 @@ class Balise : public Singleton<Balise>{
 	private:
 		volatile uint16_t max_counter_;
 		volatile uint16_t toptour_;
-		volatile bool is_toptour_available_;
 /*
 		typedef Timer<2,ModeFastPwm,1> T_2;
 		
--- software/avr/atmega2560/balise_robot/src/balise.cpp
+++ software/avr/atmega2560/balise_robot/src/balise.cpp
@@ -8,11 +8,10 @@ void Balise::max_counter(uint16_t valeur){
 	max_counter_ = valeur;
 }
 float Balise::getAngle() {
-	while(!is_toptour_available_){ asm("nop"); }
 	return ((float) toptour_)*360/((float) max_counter_);
 }
 
-Balise::Balise()  : is_toptour_available_(true)//: asservissement_moteur_(0.5,0.5,0)
+Balise::Balise() //: asservissement_moteur_(0.5,0.5,0)
 {
 	Serial<0>::init();
 	
@@ -25,22 +24,15 @@ void Balise::asservir(int32_t vitesse_courante)
 
 void Balise::incremente_toptour()
 {
-	while(!is_toptour_available_){ asm("nop"); }
-	is_toptour_available_ = false;
 	toptour_++;
-	is_toptour_available_ = true;
 }
 
 void Balise::reset_toptour()
 {
-	while(!is_toptour_available_){ asm("nop"); }
-	is_toptour_available_ = false;
 	toptour_=0;
-	is_toptour_available_ = true;
 }
 
 uint16_t Balise::toptour()
 {
-	while(!is_toptour_available_){ asm("nop"); }
 	return toptour_;
 }
