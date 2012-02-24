b023f7ba4d568d3001e1e0808415347bc00a1f69
Vincent <shalafi@vincent-pc.(none)>
2012-02-24 19:21:36 +0100

    bla

--- software/pc/lib/test.py
+++ software/pc/lib/test.py
@@ -26,14 +26,14 @@ asser.centrePython(angle)
 #Ici le centre et le rayon du robot sont initialisés, tu y accèdes avec robot.centre et robot.rayon
 #J'ai mis la suite du code, à modifier de ton côté certainement. Ma fonction centreAvr() n'est pas testée, je l'utilise pas ici du coup ! (et je vais en SH maintenant)
 
-theta = thetastar.Thetastar([])
-log.logger.info("Appel de la recherche de chemin pour le point de départ : ("+depart.x+","+depart.y+") et d'arrivée : ("+arrivee.x+","+arrivee.y+")")
-chemin_python = theta.rechercheChemin(depart,arrivee)
+theta = recherche_chemin.thetastar.Thetastar([])
+print "Appel de la recherche de chemin pour le point de départ : ("+str(depart.x)+","+str(depart.y)+") et d'arrivée : ("+str(arrivee.x)+","+str(arrivee.y)+")"
+chemin = theta.rechercheChemin(depart,arrivee)
 
 i = 0
 while i+1 < len(chemin):
-    centre_avr[i] = centrePython(chemin[i],chemin[i+1])
+    centre_avr[i] = centreAVR(chemin[i],chemin[i+1])
         
 i = 0
-for i in chemin_python:
-    print "goto " + centre_avr[i].x + ' ' + centre_avr[i].y + '\n'
+for i in chemin:
+    print "goto " + str(centre_avr[i].x) + ' ' + str(centre_avr[i].y) + '\n'
