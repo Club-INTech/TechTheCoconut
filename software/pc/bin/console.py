e3d4e119dd243c1c0b9c2a63dbd55ddbadd34f9c
Pierre de Beaucorps <pierre@debeaucorps.com>
2012-03-08 22:08:42 +0100

    Revert "La classe robot est maintenant instanciée par la VARIABLE GLOBALE robotInstance"
    This reverts commit e0fd1e6596802bce9d993f1de04c90d6299c730a.

--- software/pc/bin/console.py
+++ software/pc/bin/console.py
@@ -3,20 +3,12 @@
 import sys
 from sys import argv
 import os
-import __builtin__
 
 # Ajout de ../ au path python
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 
 import lib.chargement_lib
 log = lib.log.Log(__name__)
-
-# Création du robot
-# WARNING robotInstance est une variable globale qui instancie la classe Robot
-__builtin__.robotInstance = lib.robot.Robot()
-
-robotInstance.setPosition(lib.outils_math.point.Point(1000,1500))
-robotInstance.setOrientation(0)
 
 try:
     from IPython.Shell import IPShellEmbed
