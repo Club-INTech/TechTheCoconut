69a289c678d4992fb5b48c11b6d2b4239a400865
Philippe Tillet <Phil.Tillet@gmail.com>
2012-03-13 20:46:43 +0100

    * changement du code capteur pour supporter deux ultrasons

--- software/avr/atmega328p/capteurs/main.cpp
+++ software/avr/atmega328p/capteurs/main.cpp
@@ -4,6 +4,7 @@
 #include <avr/interrupt.h>
 #include <libintech/timer.hpp>
 #include <libintech/serial/serial_0.hpp>
+#include <libintech/ring_buffer.hpp>
 
 //Fonctions de lecture/écriture de bit
 #ifndef sbi
@@ -17,6 +18,9 @@
 #endif
 
 typedef Timer<1,ModeCounter,8> ClasseTimer;
+ring_buffer<unsigned int, 6> mesures;
+uint16_t derniere_valeur1;
+uint16_t derniere_valeur2;
 
 int main() {
 
@@ -29,6 +33,12 @@ int main() {
 	cbi(EICRA,ISC01);
 	sbi(EICRA,ISC00);
 	sbi(EIMSK,INT0);//Activation proprement dite
+	
+	cbi(DDRD,PORTD3);
+	//Activation des interruptions pour tout changement logique pour pin3
+	cbi(EICRA,ISC11);
+	sbi(EICRA,ISC10);
+	sbi(EIMSK,INT1);//Activation proprement dite
 
 	sei();
 
@@ -42,12 +52,30 @@ int main() {
 ISR(INT0_vect)
 {
 	//Front montant
-	if(rbi(PIND,PORTD2))	{
-		ClasseTimer::value(0);
+	if(rbi(PIND,PORTD2)){
+		derniere_valeur1 = ClasseTimer::value();
 	}else{//Front descendant
 		// prescaler/fcpu*inchToCm/tempsParInch
-		Serial<0>::print((uint16_t) (ClasseTimer::value()*0.0884353741496));
+		if(ClasseTimer::value() < derniere_valeur1)
+		  mesures.append(uint16_t((ClasseTimer::value() + 65536 - derniere_valeur1  )*0.0884353741496));
+		else
+		  mesures.append(uint16_t((ClasseTimer::value() - derniere_valeur1 )*0.0884353741496));
+		Serial<0>::print(mesures.data()[mesures.current() - 1]);
+	}
+}
 
+ISR(INT1_vect)
+{
+	//Front montant
+	if(rbi(PIND,PORTD3))	{
+		derniere_valeur2 = ClasseTimer::value();
+	}else{//Front descendant
+		// prescaler/fcpu*inchToCm/tempsParInch
+		if(ClasseTimer::value() < derniere_valeur2)
+		  mesures.append(uint16_t((ClasseTimer::value() + 65536 - derniere_valeur2  )*0.0884353741496));
+		else
+		  mesures.append(uint16_t((ClasseTimer::value() - derniere_valeur2 )*0.0884353741496));
+		Serial<0>::print(mesures.data()[mesures.current() - 1]);
 	}
 }
 
