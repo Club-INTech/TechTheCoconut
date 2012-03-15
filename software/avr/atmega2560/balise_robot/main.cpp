9cb35822efa33745c67ab3e39eec92fdf98a262a
Philippe Tillet <Phil.Tillet@gmail.com>
2012-03-15 18:35:04 +0100

    Ajout code compteurs

--- software/avr/atmega2560/balise_robot/main.cpp
+++ software/avr/atmega2560/balise_robot/main.cpp
@@ -23,6 +23,17 @@
 #define tbi(port,bit) (port) ^= (1 << (bit))
 #endif
 
+#ifndef rbi
+#define rbi(port,bit) ((port & (1 << bit)) >> bit)
+#endif
+
+#define READ_CANAL_A() rbi(PINB,PORTB4)
+#define READ_CANAL_B() rbi(PINB,PORTB5)
+
+volatile uint8_t dernier_etat_a;
+volatile uint8_t dernier_etat_b;
+volatile int32_t codeur;
+
 int main() {
 	ClasseTimer::init();
 	Serial<0>::init();
@@ -40,6 +51,12 @@ int main() {
 	sbi(EIMSK,INT0);//Activation proprement dite
 
 	// Initialisation interruptions codeurs
+	// Interruptions de codeuse(PCINT4 => Pin 10 sur l'Arduino)
+	sbi(PCMSK0,PCINT4);
+	// Activer les interruptions
+	sbi(PCICR,PCIE0);
+
+	// Initialisation interruptions codeurs
 	// Masques
 	//PCMSK0 |= (1 << PCINT7);
 	// Activer les interruptions
@@ -49,20 +66,25 @@ int main() {
 
 	unsigned char rawFrame[3];
 	
-	while (1) {		
-		
-		Serial<0>::read(rawFrame,4);
-		Frame frame(rawFrame);
-
-		if (frame.isValid()) {
-			
-			Serial<0>::print(frame.getRobotId());
-			Serial<0>::print(frame.getDistance());
-			//Serial<0>::print(balise.getAngle());
-		} else {
-			Serial<0>::print("ERROR");
-		}
+	dernier_etat_a = READ_CANAL_A();
+	dernier_etat_b = READ_CANAL_B();
+	
+	while (1) {
+		Serial<0>::print(codeur);
+// 		Serial<0>::print("Renald");
+// 		Serial<0>::read(rawFrame,4);
+// 		Frame frame(rawFrame);
+// 
+// 		if (frame.isValid()) {
+// 			
+// 			Serial<0>::print(frame.getRobotId());
+// 			Serial<0>::print(frame.getDistance());
+// 			//Serial<0>::print(balise.getAngle());
+// 		} else {
+// 			Serial<0>::print("ERROR");
+// 		}
 	}
+	
 }
 
 ISR(TIMER0_OVF_vect)
@@ -78,4 +100,34 @@ ISR(INT0_vect)
 	if(balise.toptour()>=100)
 		balise.max_counter(balise.toptour());
 	balise.reset_toptour();
+}
+
+ISR(PCINT0_vect)
+{
+	 if(dernier_etat_a == 0 && READ_CANAL_A() == 1){
+	   if(READ_CANAL_B() == 0)
+	     codeur--;
+	   else
+	     codeur++;
+	 }
+	 else if(dernier_etat_a == 1 && READ_CANAL_A() == 0){
+	   if(READ_CANAL_B() == 0)
+	     codeur++;
+	   else
+	     codeur--;
+	 }
+	 if(dernier_etat_b == 0 && READ_CANAL_B() == 1){
+	   if(READ_CANAL_A() == 0)
+	     codeur--;
+	   else
+	     codeur++;
+	 }
+	 else if(dernier_etat_b == 1 && READ_CANAL_B() == 0){
+	   if(READ_CANAL_A() == 0)
+	     codeur++;
+	   else
+	     codeur--;
+	 }
+	dernier_etat_a = READ_CANAL_A();
+	dernier_etat_b = READ_CANAL_B(); 
 }
