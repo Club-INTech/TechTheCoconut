4ead15792acbd6e0607ff43494b4f5ec6d3d0cd5
Thibaut REMY <thibaut.remy@telecom-sudparis.eu>
2012-03-18 13:54:08 +0100

    PC : Début d'architecture de la stratégie

--- software/pc/lib/strategie.py
+++ software/pc/lib/strategie.py
@@ -1,10 +1,26 @@
 # -*- coding: utf-8 -*-
 
 import decision
+import carte
+import robot
 
 class Strategie(decision.Decision):
     """
     Classe permettant de construire une stratégie
+    
+    :param strategie: Type de stratégie à adopter :
+                        - 1 : Stratégie en carton.
+                        - 2 : Stratégie un peu mieux.
+                        - 3 : Stratégie sopalin.
+    :type strategie:  int
+
     """
-    def __init__(self):
+    def __init__(self, strategie):
+        self.strategie = strategie
+        
+    def prendreDecision(self): 
+        """
+        Retourne la décision prise par le robot. Pour les conventions, voir /lib/decision.py
+        """
+        
         pass
