617f9e6a8f34e738224ef12e762037b1e2754492
Intech <intech@netanthoOne.lan>
2012-04-18 22:49:59 +0200

    Revert "PC : 2ème essai de joystick"
    This reverts commit 929e9e6717703ffcc11dc1a1df5e373ab5307fa4.

--- software/pc/bin/joystick.py
+++ software/pc/bin/joystick.py
@@ -6,7 +6,6 @@ import termios
 import tty
 import select
 import time
-import threading
 
 import asservissement
 import outils_math
@@ -30,13 +29,6 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 import lib.chargement_lib
 log = lib.log.Log(__name__)
 
-class Action(threading.Thread):
-    def init(self, action, valeur):
-        threading.Thread.__init__(self)
-        
-    def run(self):
-        exec('strategieInstance.gestion'+action+'('+str(valeur)+')')
-
 def isData():
         return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
 
@@ -49,21 +41,15 @@ try:
                         c = sys.stdin.read(1)
                         print c
                         if c == "r": # haut
-                            Action(Avancer, 500)
-                            #strategieInstance.gestionAvancer(10)
+                            strategieInstance.gestionAvancer(10)
                         elif c == "f": # bas
-                            Action(Avancer, -500)
-                            #strategieInstance.gestionAvancer(-10)
+                            strategieInstance.gestionAvancer(-10)
                         elif c == "g": # droite
-                            Action(Tourner, 0)
-                            #strategieInstance.gestionTourner(0)
+                            strategieInstance.gestionTourner(0)
                         elif c == "d": # gauche
-                            Action(Tourner, math.pi)
-                            #strategieInstance.gestionTourner(math.pi)
+                            strategieInstance.gestionTourner(math.pi)
                         elif c == '\x1b':         # x1b is ESC
                                 break
-                        else:
-                            strategieInstance.immobiliser()
                 time.sleep(0.01)
 
 finally:
