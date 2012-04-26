389db27d9dab0c3de42fe5d6f946a4481a84e58f
Thibaut REMY <thibaut.remy@telecom-sudparis.eu>
2012-04-26 15:30:46 +0200

    AVR : infrarouge qui ne ping pas BORDEL DE MES RDEU !

--- software/avr/atmega328p/infrarouges/main.cpp
+++ software/avr/atmega328p/infrarouges/main.cpp
@@ -1,13 +1,24 @@
-#include <avr/io.h> 
-#include <avr/interrupt.h>
+// LIBRAIRIES STANDARD
 #include <util/delay.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
 // LIBRAIRIE INTECH
 #include <libintech/serial/serial_0_interrupt.hpp>
 #include <libintech/serial/serial_0.hpp>
 
-#include "infrarouge.h"
+// #include "infrarouge.h"
 
+//Fonctions de lecture/écriture de bit (utile pour capteurs & jumper)
+#ifndef sbi
+#define sbi(port,bit) (port) |= (1 << (bit))
+#endif
+#ifndef cbi
+#define cbi(port,bit) (port) &= ~(1 << (bit))
+#endif
+#ifndef rbi
+#define rbi(port,bit) ((port & (1 << bit)) >> bit)
+#endif
 
 
 typedef Serial<0> serial_t_;
@@ -16,31 +27,32 @@ int main (void)
 { 
     serial_t_::init();
     serial_t_::change_baudrate(9600);
-    
-    
-    
-//    DDRE |= (1 << 2); // Set LED1 as output 
-//    DDRG |= (1 << 0); // Set LED2 as output 
 
-   ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // Set ADC prescalar to 128 - 125KHz sample rate @ 16MHz 
+//    ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // Set ADC prescalar to 128 - 125KHz sample rate @ 16MHz 
+// 
+//    ADMUX |= (1 << REFS0); // Set ADC reference to AVCC 
+//    ADMUX |= (1 << ADLAR); // Left adjust ADC result to allow easy 8 bit reading 
+// 
+//    // No MUX values needed to be changed to use ADC0 
+//     
+//    ADCSRA |= (1 << 5);  // Set ADC to Free-Running Mode 
+//    ADCSRA |= (1 << ADEN);  // Enable ADC 
+//    ADCSRA |= (1 << ADSC);  // Start A2D Conversions 
 
-   ADMUX |= (1 << REFS0); // Set ADC reference to AVCC 
-   ADMUX |= (1 << ADLAR); // Left adjust ADC result to allow easy 8 bit reading 
+   while (1)  // Loop Forever 
+   {
+       serial_t_::print(5);
+        char buffer[17];
+        serial_t_::read(buffer,17);
+        #define COMPARE_BUFFER(string,len) strncmp(buffer, string, len) == 0 && len>0
 
-   // No MUX values needed to be changed to use ADC0 
-    
-   ADCSRA |= (1 << 5);  // Set ADC to Free-Running Mode 
-   ADCSRA |= (1 << ADEN);  // Enable ADC 
-   ADCSRA |= (1 << ADSC);  // Start A2D Conversions 
-
-   for(;;)  // Loop Forever 
-   { 
-        serial_t_::print(ADCH);
-
-        serial_t_::print(conversion(ADCH));
-        serial_t_::print("***********");
-        _delay_ms(100);   
-   } 
+        // Ping
+        if(COMPARE_BUFFER("a", 1)){
+            serial_t_::print(4);
+        }
+   }
+   
+   return 1;
 
 } 
 
--- software/avr/atmega328p/infrarouges/src/infrarouge.cpp
+++ software/avr/atmega328p/infrarouges/src/infrarouge.cpp
@@ -2,22 +2,14 @@
 #include "infrarouge.h"
 
 
-#define NB_VAL_ECHANTILLONAGE  7
-uint16_t    val_ADCH[]   = {150, 120, 100, 80,  50 , 30 , 20 };
-uint16_t    val_mm[]     = {70 , 100, 120, 160, 280, 500, 650};
+#define NB_VAL_ECHANTILLONAGE  8
+uint16_t    val_ADCH[]   = {170, 150, 120, 100, 80,  50 , 30 , 20 };
+uint16_t    val_mm[]     = {50 , 70 , 100, 130, 160, 280, 500, 650};
 
 uint16_t conversion(uint16_t adch)
 {
-    if (adch < 20)
-    {
-        return 700;
-    }
-    
-    else
-    {
         uint8_t ind = indiceTab(adch);
         return regLin(val_ADCH[ind], val_ADCH[ind+1], val_mm[ind], val_mm[ind+1], adch);        
-    }
 }
 
 
@@ -25,7 +17,7 @@ uint16_t indiceTab(uint16_t adch)
 {
     uint16_t i;
     
-    for (i = 1; i < NB_VAL_ECHANTILLONAGE; i++)
+    for (i = 1; i < NB_VAL_ECHANTILLONAGE -1; i++)
     {
         if (adch >= val_ADCH[i])
         {
@@ -40,5 +32,6 @@ uint16_t indiceTab(uint16_t adch)
 uint16_t regLin(uint16_t x1, uint16_t x2, uint16_t y1, uint16_t y2, uint16_t x)
 {
     uint16_t pourcentage = ((x1-x)*100)/(x1-x2);
-    return (1-pourcentage/100)*y2 + pourcentage*y1/100;
+//     return pourcentage;
+    return (pourcentage*y2 + (100-pourcentage)*y1)/100;
 }
