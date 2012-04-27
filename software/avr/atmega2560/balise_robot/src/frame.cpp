6cdc4f0389659d7b3bbe64795bff58a837a0b64a
Paul <paul.bernier@it-sudparis.eu>
2012-04-27 22:41:09 +0200

    dernière chance... Please enter the commit message for your changes. Lines starting

--- software/avr/atmega2560/balise_robot/src/frame.cpp
+++ software/avr/atmega2560/balise_robot/src/frame.cpp
@@ -6,7 +6,7 @@
 #include <libintech/serial/serial_0.hpp>
 
 Frame::Frame(uint32_t rawFrame) {
-	rawframe_=rawframe;
+	rawframe_=rawFrame;
 	distance_ = (uint16_t) (rawFrame >> 20);//12 bits de poids les plus forts
 	offset_ = ((uint16_t) (rawFrame >> 8)) && 0b0000111111111111 ;//12 bits entre
 	crc_ = (uint8_t) (rawFrame);//8 bits de poids les plus faible	
