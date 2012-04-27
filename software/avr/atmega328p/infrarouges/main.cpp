707fff0b9c5d7fbbb5718e3728a40ed8e94d128c
Thibaut REMY <thibaut.remy@telecom-sudparis.eu>
2012-04-27 18:22:06 +0200

    AVR : infrarouge : On attends plus qu'un commit de @Ronald.

--- software/avr/atmega328p/infrarouges/main.cpp
+++ software/avr/atmega328p/infrarouges/main.cpp
@@ -7,7 +7,8 @@
 #include <libintech/serial/serial_0_interrupt.hpp>
 #include <libintech/serial/serial_0.hpp>
 
-// #include "infrarouge.h"
+// LIBRAIRIE LOCALE
+#include "infrarouge.h"
 
 //Fonctions de lecture/écriture de bit (utile pour capteurs & jumper)
 #ifndef sbi
@@ -28,29 +29,29 @@ int main (void)
     serial_t_::init();
     serial_t_::change_baudrate(9600);
 
-//    ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // Set ADC prescalar to 128 - 125KHz sample rate @ 16MHz 
-// 
-//    ADMUX |= (1 << REFS0); // Set ADC reference to AVCC 
-//    ADMUX |= (1 << ADLAR); // Left adjust ADC result to allow easy 8 bit reading 
-// 
-//    // No MUX values needed to be changed to use ADC0 
-//     
-//    ADCSRA |= (1 << 5);  // Set ADC to Free-Running Mode 
-//    ADCSRA |= (1 << ADEN);  // Enable ADC 
-//    ADCSRA |= (1 << ADSC);  // Start A2D Conversions 
+   ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // Set ADC prescalar to 128 - 125KHz sample rate @ 16MHz 
+
+   ADMUX |= (1 << REFS0); // Set ADC reference to AVCC 
+   ADMUX |= (1 << ADLAR); // Left adjust ADC result to allow easy 8 bit reading 
+
+   // No MUX values needed to be changed to use ADC0 
+    
+   ADCSRA |= (1 << 5);  // Set ADC to Free-Running Mode 
+   ADCSRA |= (1 << ADEN);  // Enable ADC 
+   ADCSRA |= (1 << ADSC);  // Start A2D Conversions 
 
    while (1)  // Loop Forever 
    {
-       serial_t_::print(5);
         char buffer[17];
         serial_t_::read(buffer,17);
         #define COMPARE_BUFFER(string,len) strncmp(buffer, string, len) == 0 && len>0
-
+        
         // Ping
-        if(COMPARE_BUFFER("a", 1)){
-            serial_t_::print(4);
+        if(COMPARE_BUFFER("infra", 5)){
+            serial_t_::print(conversion(ADCH));
         }
-   }
+
+   }    
    
    return 1;
 
