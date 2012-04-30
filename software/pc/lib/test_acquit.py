4a003615dad4ed27513dcaa01848243136d74d80
Pierre de Beaucorps <pierre@debeaucorps.com>
2012-04-30 18:05:11 +0200

    pc : instanciation proof

--- software/pc/lib/test_acquit.py
+++ software/pc/lib/test_acquit.py
@@ -13,8 +13,11 @@ import math
 from outils_math.point import Point
 
 
-asserInstance = __builtin__.instance.asserInstance
-actionInstance = __builtin__.instance.actionInstance
+if hasattr(__builtin__.instance, 'asserInstance'):
+    asserInstance = __builtin__.instance.asserInstance
+if hasattr(__builtin__.instance, 'actionInstance'):
+    actionInstance = __builtin__.instance.actionInstance
+    
 
 a = Point(200.0,200.0)
 b = Point(300.0,500.0) 
@@ -46,63 +49,63 @@ def console():
     asserInstance.setPosition(Point(0,400))
     
     while True:
-        try:
-            ordre = raw_input(">>")
-            if ordre == "?" or ordre == "exit" :
-                break
-            elif ordre == "g":
-                x = raw_input("x ?")
-                y = raw_input("y ?")
-                dest = Point(float(x),float(y)) 
-                asserInstance.goTo(dest)
-            elif ordre == "q":
-                asserInstance.gestionTourner(3.14)
-            elif ordre == "s":
-                asserInstance.gestionTourner(-1.57)
-            elif ordre == "d":
-                asserInstance.gestionTourner(0)
-            elif ordre == "z":
-                asserInstance.gestionTourner(1.57)
-                
-            elif ordre == "u":
-                asserInstance.gestionAvancer(100)
-            elif ordre == "j":
-                asserInstance.gestionAvancer(-100)
-                
-            elif ordre == "i":
-                asserInstance.gestionAvancer(200)
-            elif ordre == "k":
-                asserInstance.gestionAvancer(-200)
-                
-            elif ordre == "o":
-                asserInstance.gestionAvancer(300)
-            elif ordre == "l":
-                asserInstance.gestionAvancer(-300)
-                
-            elif ordre == "p":
-                asserInstance.gestionAvancer(400)
-            elif ordre == "m":
-                asserInstance.gestionAvancer(-400)
-                
-            elif ordre == "n":
-                actionInstance.deplacer(180)
-            elif ordre == "b":
-                actionInstance.deplacer(130)
-            elif ordre == "v":
-                actionInstance.deplacer(120)
-            elif ordre == "c":
-                actionInstance.deplacer(110)
-            elif ordre == "x":
-                actionInstance.deplacer(100)
-            elif ordre == "w":
-                actionInstance.deplacer(0)
-            else:
-                try:
-                    actionInstance.deplacer(int(ordre))
-                except:
-                    pass
-        except:
-            print "--- exception levée ---"
+        #try:
+        ordre = raw_input(">>")
+        if ordre == "?" or ordre == "exit" :
+            break
+        elif ordre == "g":
+            x = raw_input("x ?")
+            y = raw_input("y ?")
+            dest = Point(float(x),float(y)) 
+            asserInstance.goTo(dest)
+        elif ordre == "q":
+            asserInstance.gestionTourner(3.14)
+        elif ordre == "s":
+            asserInstance.gestionTourner(-1.57)
+        elif ordre == "d":
+            asserInstance.gestionTourner(0)
+        elif ordre == "z":
+            asserInstance.gestionTourner(1.57)
+            
+        elif ordre == "u":
+            asserInstance.gestionAvancer(100)
+        elif ordre == "j":
+            asserInstance.gestionAvancer(-100)
+            
+        elif ordre == "i":
+            asserInstance.gestionAvancer(200)
+        elif ordre == "k":
+            asserInstance.gestionAvancer(-200)
+            
+        elif ordre == "o":
+            asserInstance.gestionAvancer(300)
+        elif ordre == "l":
+            asserInstance.gestionAvancer(-300)
+            
+        elif ordre == "p":
+            asserInstance.gestionAvancer(400)
+        elif ordre == "m":
+            asserInstance.gestionAvancer(-400)
+            
+        elif ordre == "n":
+            actionInstance.deplacer(180)
+        elif ordre == "b":
+            actionInstance.deplacer(130)
+        elif ordre == "v":
+            actionInstance.deplacer(120)
+        elif ordre == "c":
+            actionInstance.deplacer(110)
+        elif ordre == "x":
+            actionInstance.deplacer(100)
+        elif ordre == "w":
+            actionInstance.deplacer(0)
+        else:
+            try:
+                actionInstance.deplacer(int(ordre))
+            except:
+                pass
+        #except:
+            #print "--- exception levée ---"
 
 console()
         
