bdc1b81d4cbda2807ca1d125b38eee5b5425a731
Thibaut REMY <thibaut.remy@telecom-sudparis.eu>
2012-05-05 18:20:17 +0200

    libintech : Register : Les accolades étaient pas si inutiles que ça après coup.

--- software/avr/library/libintech/register.hpp
+++ software/avr/library/libintech/register.hpp
@@ -4,62 +4,78 @@
 #include <stdint.h>
 #include <avr/io.h>
 
+/// PORT D
 template<uint16_t bit>
 struct AVR_PORTD
 {
-    static void set_input()
-        DDRD  |= (1 << bit);
-    
-    static void set_output()
+    static void set_input(){
+        DDRD |= (1 << bit);
+    }
+    static void set_output(){
         DDRD &= ~(1 << bit);
-    
-    static void set()
+    }
+    static void set(){
         PORTD |= (1 << bit);
-        
-    static void clear()
+    }
+    static void clear(){
         PORTD &= ~(1 << bit);
-        
-    static uint8_t read()
+    }
+    static uint8_t read(){
         return ( (PIND & (1 << bit)) >> bit );
+    }
 };
 
+/// PORT C
 template<uint16_t bit>
 struct AVR_PORTC
 {
-    static void set_input()
+    static void set_input(){
         DDRC  |= (1 << bit);
+    }
     
-    static void set_output()
+    static void set_output(){
         DDRC &= ~(1 << bit);
+    }
     
-    static void set()
+    static void set(){
         PORTC |= (1 << bit);
-        
-    static void clear()
+    }
+    
+    static void clear(){
         PORTC &= ~(1 << bit);
-        
-    static uint8_t read()
+    }
+    
+    static uint8_t read(){
         return ( (PINC & (1 << bit)) >> bit );
+    }
+    
 };
 
+
+/// PORT B
 template<uint16_t bit>
 struct AVR_PORTB
 {
-    static void set_input()
+    static void set_input(){
         DDRB  |= (1 << bit);
+    }
     
-    static void set_output()
+    static void set_output(){
         DDRB &= ~(1 << bit);
+    }
     
-    static void set()
+    static void set(){
         PORTB |= (1 << bit);
-        
-    static void clear()
+    }
+    
+    static void clear(){
         PORTB &= ~(1 << bit);
-        
-    static uint8_t read()
+    }
+    
+    static uint8_t read(){
         return ( (PINB & (1 << bit)) >> bit );
-
+    }
+    
 };
 
 #endif /* REGISTER_HPP_ */
