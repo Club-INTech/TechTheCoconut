f7dad7979dc16222f3ddbb445e9e550f0971b2b7
Thibaut REMY <thibaut.remy@telecom-sudparis.eu>
2012-05-05 18:07:02 +0200

    libintech : Register : Ajout du port C et enlevage des { } useless.

--- software/avr/library/libintech/register.hpp
+++ software/avr/library/libintech/register.hpp
@@ -5,48 +5,61 @@
 #include <avr/io.h>
 
 template<uint16_t bit>
-struct AVR_PORTD{
+struct AVR_PORTD
+{
     static void set_input()
-    {
         DDRD  |= (1 << bit);
-    }
     
     static void set_output()
-    {
         DDRD &= ~(1 << bit);
-    }
     
-	static void set(){
-		PORTD |= (1 << bit);
-	}
-	static void clear(){
-		PORTD &= ~(1 << bit);
-	}
-	static uint8_t read(){
-		return ( (PIND & (1 << bit)) >> bit );
-	}
+    static void set()
+        PORTD |= (1 << bit);
+        
+    static void clear()
+        PORTD &= ~(1 << bit);
+        
+    static uint8_t read()
+        return ( (PIND & (1 << bit)) >> bit );
 };
 
 template<uint16_t bit>
-struct AVR_PORTB{
+struct AVR_PORTC
+{
     static void set_input()
-    {
-        DDRB  |= (1 << bit);
-    }
+        DDRC  |= (1 << bit);
     
     static void set_output()
-    {
+        DDRC &= ~(1 << bit);
+    
+    static void set()
+        PORTC |= (1 << bit);
+        
+    static void clear()
+        PORTC &= ~(1 << bit);
+        
+    static uint8_t read()
+        return ( (PINC & (1 << bit)) >> bit );
+};
+
+template<uint16_t bit>
+struct AVR_PORTB
+{
+    static void set_input()
+        DDRB  |= (1 << bit);
+    
+    static void set_output()
         DDRB &= ~(1 << bit);
-    }
-	static void set(){
-		PORTB |= (1 << bit);
-	}
-	static void clear(){
-		PORTB &= ~(1 << bit);
-	}
-	static uint8_t read(){
-		return ( (PINB & (1 << bit)) >> bit );
-	}
+    
+    static void set()
+        PORTB |= (1 << bit);
+        
+    static void clear()
+        PORTB &= ~(1 << bit);
+        
+    static uint8_t read()
+        return ( (PINB & (1 << bit)) >> bit );
+
 };
 
 #endif /* REGISTER_HPP_ */
