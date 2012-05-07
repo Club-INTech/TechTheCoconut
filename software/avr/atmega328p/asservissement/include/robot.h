c1423125c3eeca093708328d74f1d91d5be6d55b
Pierre de Beaucorps <pierre@debeaucorps.com>
2012-05-07 03:34:33 +0200

    AVR : version bande d'arcade

--- software/avr/atmega328p/asservissement/include/robot.h
+++ software/avr/atmega328p/asservissement/include/robot.h
@@ -33,69 +33,13 @@ private:
 	TimerCounter_t compteur;
 	
 	typedef Serial<0> serial_t_;
-	
-	unsigned char couleur_;
-	float x_;
-	float y_;
-	float angle_serie_;
-	float angle_origine_;
-	void changer_orientation(float new_angle);
-	
-	bool etat_rot_;
-	bool etat_tra_;
-	bool est_bloque_;
-	
-	int32_t mesure_distance_;
-	int32_t mesure_angle_;
-	
-	Asservissement translation;
-	Asservissement rotation;
-
-	float CONVERSION_TIC_MM_;
-	float CONVERSION_TIC_RADIAN_;
+	int32_t pwmG_;
+	int32_t pwmD_;
 	
 public:
-	
 	Robot();
-	
-	void couleur(unsigned char);
-	unsigned char couleur(void);
-	void x(float);
-	float x(void);
-	void y(float);
-	float y(void);
-	
-	//gestion des mesures courantes
-	void mesure_angle(int32_t); 
-	int32_t mesure_angle(void);
-	void mesure_distance(int32_t); 
-	int32_t mesure_distance(void);
-	void envoyer_position_tic(void);
-	bool est_stoppe();
-	
-// 	void acquittement();
-	
-	void asservir();
-	void update_position();
+	void bandeArcade();
 	void communiquer_pc();
-	
-	int32_t angle_initial(void);
-// 	float angle_optimal(float angle, float angleBkp);
-	int32_t angle_optimal(int32_t angle, int32_t angleBkp);
-	int32_t compare_angle_tic(int32_t angle1,int32_t angle2);
-	
-	void envoyer_position(void);
-	
-	void tourner(float angle);
-	void translater(float distance);
-
-	void stopper();
-	void gestion_blocage();
-	
-	void recalage(void);
-	
-	void translater_bloc(float distance);
-	void tourner_bloc(float angle);
 };
 
 #endif
