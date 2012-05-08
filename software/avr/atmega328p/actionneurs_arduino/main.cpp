4abb1301688e3b316340a6a3865988ed09413e0b
Thibaut REMY <thibaut.remy@telecom-sudparis.eu>
2012-05-08 08:50:37 +0200

    AVR : Actionneurs Arduino : Lib 324p ->> Lib 328p

--- software/avr/atmega328p/actionneurs_arduino/main.cpp
+++ software/avr/atmega328p/actionneurs_arduino/main.cpp
@@ -25,18 +25,18 @@
 // Mode pour reflasher l'id des AX12 connectés. Attention à c'qu'on fait.
 // Si ce mode est positif, il reflashera l'id des AX12 à la valeur du mode.
 // Pour ne pas reflasher l'id des AX12 connectés, mettre une valeur négative.
-    #define FLASH_ID_MODE           -1
+    #define FLASH_ID_MODE           2
 
 // Mode pour reflasher le baud rate d'écoute des AX12. Warning. Achtung.
 // Mettre à 0 pour ne pas le reflasher, à 1 sinon. Si ce mode est à 1,
 // la carte reflashera le baud rate d'écoute de l'AX12 à la valeur
 // BAUD_RATE_AX12 (définie un peu plus haut)
-    #define FLASH_BAUD_RATE_MODE    0
+    #define FLASH_BAUD_RATE_MODE    1
 
 // Mode pour tester les AX12 sans utiliser la liaison PC.
 // A mettre à 1 pour l'utiliser, à 0 sinon. Si le mode est mis à 1, l'AX12
 // tournera en continu.
-    #define TEST_NOSERIE_MODE       0
+    #define TEST_NOSERIE_MODE       1
 
 // Mode si l'AX12 ne répond pas alors qu'il le devrait. Vérifier la masse.
 // Si ce mode est utilisé, les diodes de l'AX12 clignotent 5 sec après un
@@ -65,7 +65,7 @@ int main()
     byte debug_baudrate = 0x00;
         
     // BAUD RATE de la série (envoi)
-    ax12Init(BAUD_RATE_SERIE);
+    AX12_Serial_Init(BAUD_RATE_SERIE);
     
     if (FLASH_BAUD_RATE_MODE)
         // BAUD RATE de l'AX12 (réception)
@@ -75,25 +75,27 @@ int main()
         AX12InitID(FLASH_ID_MODE);
         
     // Initialisation de tous les AX12
-    AX12Init (AX_BROADCAST, AX_ANGLECW, AX_ANGLECCW, AX_SPEED); 
-        
+    AX12Init (AX_BROADCAST, AX_ANGLECW, AX_ANGLECCW, AX_SPEED);
+    
+    
         
     while (1)
-    {
         
+    {
         if (REANIMATION_MODE)
         {
-            ax12Init(2000000/(debug_baudrate + 1));
+            AX12_Serial_Init(2000000/(debug_baudrate + 1));
             reset(0xFE);
             debug_baudrate++;
         }
         
         else if (TEST_NOSERIE_MODE) 
-            AX12Init(0xFE, 0,0,1200);
+        {
+            AX12GoTo(0xFE, AX_ANGLECW + (int16_t)(600.*90/180.));
+        }
         
         else
         {
-            
             char buffer[17];
             serial_t_::read(buffer,17);
             #define COMPARE_BUFFER(string,len) strncmp(buffer, string, len) == 0 && len>0
@@ -102,31 +104,126 @@ int main()
             if(COMPARE_BUFFER("?", 1))
                 serial_t_::print(4);
 
-            
             // GoTo angle
             else if (COMPARE_BUFFER("GOTO", 4))
             {
                 int8_t id = serial_t_::read_int();
                 int16_t angle = serial_t_::read_int();
+
                 AX12GoTo(id, AX_ANGLECW + (int16_t)(600.*angle/180.));
             }
-        
-            // Changement de vitesse
-            else if (COMPARE_BUFFER("CH_VITESSE", 10))
+            
+            // Goto Broadcast
+            else if (COMPARE_BUFFER("g", 1))
             {
-                int16_t speed = serial_t_::read_int();
-                AX12Init(AX_BROADCAST, AX_ANGLECW, AX_ANGLECCW , speed);
+                int16_t angle = serial_t_::read_int();
+                
+                AX12GoTo(0xFE, AX_ANGLECW + (int16_t)(600.*angle/180.));
             }
             
-            // Désasservissement de tous les servos branchés
-            else if (COMPARE_BUFFER("UNASSERV", 8))
-                AX12Unasserv(0xFE);
+            // Goto brut
+            else if (COMPARE_BUFFER("a", 1))
+            {
+                int8_t id = serial_t_::read_int();
+                int16_t angle = serial_t_::read_int();
+                AX12GoTo(id, angle);
+            }
+            
+            else if (COMPARE_BUFFER("z", 1))
+            {
+                
+                AX12GoTo(0xFE, AX_ANGLECW + (int16_t)(600.*80/180.));
+                serial_t_::print("z");
+            }
+            
+            else if (COMPARE_BUFFER("e", 1))
+            {
+                
+                AX12GoTo(0xFE, AX_ANGLECW + (int16_t)(600.*100/180.));
+                serial_t_::print("e");
+            }
+            
+            // Changement de vitesse
+            else if (COMPARE_BUFFER("CH_VIT", 6))
+            {
+                int8_t  id    = serial_t_::read_int();
+                int16_t speed = serial_t_::read_int();
+                AX12ChangeSpeed(id, speed);
+            }
+            
+            // Changement de vitesse broadcast
+            else if (COMPARE_BUFFER("c", 1))
+            {
+                int16_t speed = serial_t_::read_int();
+                AX12ChangeSpeed(0xFE, speed);
+            }
+            
+            // Changement de l'angleCW (min)
+            else if (COMPARE_BUFFER("m", 1))
+            {
+                int16_t angle = serial_t_::read_int();
+                AX12ChangeAngleMIN(0xFE, angle);
+            }
+            
+            else if (COMPARE_BUFFER("M", 1))
+            {
+                int16_t angle = serial_t_::read_int();
+                AX12ChangeAngleMAX(0xFE, angle);
+            }
+               
             
             // Reflashage de tous les servos branchés
-            else if (COMPARE_BUFFER("FLASH_ID", 8))
+            else if (COMPARE_BUFFER("f", 8))
             {
                 int8_t id = serial_t_::read_int();
                 AX12InitID(id);
+            }
+            
+            // Désasservissement de tous les servos branchés
+            else if (COMPARE_BUFFER("UNASSERV", 8) || COMPARE_BUFFER("u", 1) || COMPARE_BUFFER("", 0))
+                AX12Unasserv(0xFE);
+            
+            else if (COMPARE_BUFFER("U", 1))
+            {
+                uint8_t id = serial_t_::read_int();
+                AX12Unasserv(id);
+            }
+            
+            // Changement de T° MAX.
+            else if (COMPARE_BUFFER("t", 1))
+            {
+                writeData(0xFE, AX_LIMIT_TEMPERATURE, 1, 120);
+                writeData(0xFE, AX_UP_LIMIT_VOLTAGE,  1, 180);
+                serial_t_::print("ok");
+            }
+
+            // LEDs d'alarme.
+            else if (COMPARE_BUFFER("LED", 3))
+            {
+                uint8_t type = serial_t_::read_int();
+                
+                writeData(0xFE, AX_ALARM_LED, 1, type);
+                serial_t_::print("ok");
+            }
+            
+            // Message générique.
+            else if (COMPARE_BUFFER("MESS", 4))
+            {
+                // On lit l'id
+                uint8_t id = serial_t_::read_int();
+                
+                // On lit l'adresse de début
+                uint8_t adresse = serial_t_::read_int();
+                
+                // On lit le nombre d'octets
+                uint8_t n = serial_t_::read_int();
+                
+                // On lit la valeur à écrire
+                uint16_t val = serial_t_::read_int();
+                
+                writeData(id, adresse, n, val);
+                
+                serial_t_::print("ok");
             }
             
         }
--- software/avr/atmega328p/actionneurs_arduino/src/actionneurs.c
+++ software/avr/atmega328p/actionneurs_arduino/src/actionneurs.c
@@ -1,20 +1,21 @@
 #include "actionneurs.h"
 #include "ax12.h"
 
+/** @file avr/atmega324p/actionneurs/include/actionneurs.cpp
+ *  @brief Ce fichier crée les constantes haut niveau pour les actionneurs.
+ *  @author Thibaut ~MissFrance~
+ *  @date 05 mai 2012
+ */ 
+// Librairie INTech
 #include <libintech/serial/serial_0.hpp>
 
-
+/// Initialisation de l'ID de tous les AX12 branchés.
 void AX12InitID(uint8_t ID)
 {
-    writeData (1, AX_ID, 1, ID);
+    writeData (0xFE, AX_ID, 1, ID);
 }
 
-void AX12SetLeds(uint8_t ID, byte masque)
-{
-    writeData(ID, AX_LED, 1, masque);
-    writeData(ID, AX_ALARM_LED, 1, masque);
-}
-
+/// Initialisation de l'AX12
 void AX12Init (uint8_t ID, uint16_t angleCW, uint16_t angleCCW, uint16_t vitesse)
 {
     // Active l'asservissement du servo
@@ -32,23 +33,19 @@ void AX12GoTo (uint8_t ID, uint16_t angle)
     writeData (ID, AX_GOAL_POSITION_L, 2, angle);
 }
 
-void AX12GOTO(uint8_t ID, uint16_t angle)
+void AX12ChangeAngleMIN(uint8_t ID, uint16_t angleCW)
 {
-    Serial<0>::print(0xFF);
-    Serial<0>::print(0xFF);
-        
+    writeData (ID, AX_CW_ANGLE_LIMIT_L, 2, angleCW);
 }
 
-void extraction(unsigned char x, char *x1, char *x2)
+void AX12ChangeAngleMAX(uint8_t ID, uint16_t angleCCW)
 {
-    char tmp = x >> 5;
-    *x1 = tmp & 0b00000011;
-    *x2 = x & 0b00011111;
+    writeData (ID, AX_CCW_ANGLE_LIMIT_L, 2, angleCCW);
 }
 
-void extraction_consigne(unsigned char x, char *cons)
+void AX12ChangeSpeed(uint8_t ID, uint16_t vitesse)
 {
-    *cons = x >> 7;
+    writeData (ID, AX_GOAL_SPEED_L, 2, vitesse);
 }
 
 void AX12Unasserv(uint8_t ID)
