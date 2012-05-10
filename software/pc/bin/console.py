0d943555f97e71b2a7c2794568586886ce186fcd
Pierre de Beaucorps <pierre@debeaucorps.com>
2012-05-10 23:02:47 +0200

    ipython #2

--- software/pc/bin/console.py
+++ software/pc/bin/console.py
@@ -10,15 +10,11 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 import lib.chargement_lib
 log = lib.log.Log(__name__)
 
-#try:
-
-print "############################\n###########################\n"
-print "#1"
-from IPython.Shell import IPShellEmbed
-print "#2"
-ipshell = IPShellEmbed()
-print "#3"
-ipshell()
-#except:
-    #log.logger.error("La dépendance Ipython n'est pas installée. Taper sudo apt-get install ipython")
+try:
+    from IPython.Shell import IPShellEmbed
+    ipshell = IPShellEmbed()
+    ipshell()
+except:
+    log.logger.error("La dépendance Ipython n'est pas installée. Taper sudo apt-get install ipython")
+    print "si vous êtes sous BeagleBoard, executez /usr/bin/ipython lanceur.py"
 
