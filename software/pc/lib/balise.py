07fbd3d4e52f71f1d93c8cf94b4d209c7c379260
Philippe <philippe@philippe-XPS-M1530.(none)>
2012-05-10 22:04:00 +0200

    Ajout matrice Q au Kalman

--- software/pc/lib/balise.py
+++ software/pc/lib/balise.py
@@ -32,13 +32,16 @@ class Balise:
             log.logger.error("balise : ne peut importer instance.serieBaliseInstance")
         self.visualisation = visualisation
         #self.serial = serial.Serial(peripherique.chemin_de_peripherique("balise"),"balise",9600,5)
-        self.dt = 0.1
-        x = numpy.array([463.,402.,0.,0.])[:, numpy.newaxis] #Vecteur d'état au départ
-        P =  numpy.matrix([[100.,0.,0.,0.],[0.,100.,0.,0.],[0.,0.,100.,0.],[0.,0.,0.,100.]])# initial uncertainty
+        self.dt = 0.2
+        self.timeout_serie = 0.25
+        x = numpy.array([1500+687.90,1000-158.81,0.,0.])[:, numpy.newaxis] #Vecteur d'état au départ
+        P =  numpy.matrix([[50.,0.,0.,0.],[0.,50.,0.,0.],[0.,0.,10.,0.],[0.,0.,0.,10.]])# initial uncertainty
         F =  numpy.matrix([[1.,0.,self.dt,0.],[0.,1.,0.,self.dt],[0.,0.,1.,0.],[0.,0.,0.,1.]])# next state function
         H =  numpy.matrix([[1.,0.,0.,0.],[0.,1.,0.,0.]])# measurement function
-        R =   numpy.matrix([[30,0.],[0.,30]])# measurement uncertainty - 10mm
-        self.filtre_kalman = FiltreKalman(x,P,F,H,R)
+        R =   numpy.matrix([[10,0.],[0.,10]])# measurement uncertainty - 10mm
+        Q = numpy.matrix([[self.dt**3/3., self.dt**2/2., 0, 0],[self.dt**2/2.,self.dt, 0, 0],[0,0,self.dt**3/3.,self.dt**2/2],[0,0,self.dt**2/2,self.dt]])
+        Q *= 10;
+        self.filtre_kalman = FiltreKalman(x,P,F,H,R,Q)
         self.mutex = Lock()
     
     def tracker_robot_adverse(self):
@@ -53,6 +56,8 @@ class Balise:
                log.logger.error("balise : canal pourri.")
             elif(retour == "NON_VISIBLE"):
                 log.logger.error("balise : non visible.")
+            elif(retour == "timeout"):
+                pass
             else:
                 log.logger.info("Balise : Trame intègre")
                 vals = retour.split('.')
@@ -62,12 +67,16 @@ class Balise:
                 angle_rad = int(angle)*math.pi/180
                 x = int(dist)*math.cos(angle_rad)
                 y = int(dist)*math.sin(angle_rad)
-                log.logger.info("Balise : Mise à jour des coordonnées par Kalman : " + str(x) + ", " + str(y) )
-                self.filtre_kalman.filtrer(numpy.array([x+1500,y+1000])[:, numpy.newaxis])
+                new_x = 1500+x;
+                new_y = 1000+y;
+                log.logger.info("Balise : Mise à jour des coordonnées par Kalman : " + str(new_x) + ", " + str(new_y) )
+                self.filtre_kalman.filtrer(numpy.array([new_x,new_y])[:, numpy.newaxis])
                 state = self.filtre_kalman.x;
                 if self.visualisation != None:
                     nouvelle_pos = [ state[0], state[1] ]
                     nouvelle_vitesse = [ state[2], state[3] ]
+                    #nouvelle_pos = [new_x, new_y]
+                    #nouvelle_vitesse = [0, 0]
                     self.visualisation.ajouter_pos_adversaire(nouvelle_pos)
                     self.visualisation.modifierVitesseAdversaire(nouvelle_vitesse)
                 print state
