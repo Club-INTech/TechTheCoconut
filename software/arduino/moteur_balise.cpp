1aabcbd111233bd9ab7b04340b8f1bd627194c33
Philippe <philippe@philippe-XPS-M1530.(none)>
2012-05-12 20:55:17 +0200

    Ajout fichier arduino d'asservissement du moteur de la balise

---
+++ software/arduino/moteur_balise.cpp
@@ -0,0 +1,38 @@
+#define p1 10
+#define p2 11
+#define top 6
+
+#define PERIOD 60 //1000/17Hz
+
+int cmd = 10;//commande de départ
+
+void setup(){
+  pinMode(p1, OUTPUT);
+  pinMode(p2, OUTPUT);
+  pinMode(top, INPUT);
+  Serial.begin(9600);
+
+  digitalWrite(p1,LOW);
+  analogWrite(p2,cmd);
+}
+
+
+void loop(){
+
+  long total=0;
+  long period=0;
+  
+  total+=pulseIn(top,LOW);
+  total+=pulseIn(top,HIGH);
+  period = total/1000.;
+  
+  if((period>PERIOD && cmd<250) || period==0) //0=timeout
+    cmd++;
+  if(period<PERIOD && cmd>0)
+    cmd-=1;  
+  
+  analogWrite(p2,cmd);
+  Serial.println(cmd);
+  Serial.println(period);
+  delay(300);
+}
