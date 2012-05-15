eeef01c4c595997c3d4ff5ae2beb41cbde35e40c
Pierre de Beaucorps <pierre@debeaucorps.com>
2012-05-15 23:05:11 +0200

    recherche de chemin sans recalcul en fonction des bras. calcul du point accessible le plus proche.

--- software/pc/lib/actionneur.py
+++ software/pc/lib/actionneur.py
@@ -238,7 +238,8 @@ class Actionneur(serie.Serie):
         rayon_avec_bras = math.sqrt((sommet_bras.x) ** 2 + (sommet_bras.y) ** 2)
         
         #mettre à jour l'attribut du robot
-        self.robotInstance.changeRayon(max(rayon_avec_bras,rayon_original))
+        #NOTE : on n'utilisera la recherche de chemin qu'avec des bras fermés. donc pas de recalculs intempestifs.
+        #self.robotInstance.changeRayon(max(rayon_avec_bras,rayon_original))
         
     def test(self, temps = 0.3) :
         i = 0
