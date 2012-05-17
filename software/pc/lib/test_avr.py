467f6b7ab70e41d9e91a4d1675703a1dea534815
Pierre de Beaucorps <pierre@debeaucorps.com>
2012-05-18 00:13:46 +0200

    scripts de test

---
+++ software/pc/lib/test_avr.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import __builtin__
+import instance
+
+asser = __builtin__.instance.asserInstanceDuree
+sc = __builtin__.instance.scriptInstance
+
+sc.gestionScripts(sc.recalage)
+sc.gestionScripts(sc.secour)
