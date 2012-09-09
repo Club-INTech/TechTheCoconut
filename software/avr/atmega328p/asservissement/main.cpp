49587ac72ef323ba0fb079b008d86ca51cd8aff3
Pierre de Beaucorps <pierre@debeaucorps.com>
2012-09-09 17:40:23 +0200

    commentaires lulz

--- software/avr/atmega328p/asservissement/main.cpp
+++ software/avr/atmega328p/asservissement/main.cpp
@@ -34,14 +34,19 @@ ISR(TIMER1_OVF_vect, ISR_NOBLOCK){
 		robot.bandeArcade();
 	else
 	{
+		//mise à jour des attribut stockant la distance parcourue en tic et l'angle courant en tic
 		int32_t infos[2];
-		//info[0]=>distance courante ; info[1] => angle courant.
 		get_all(infos);
-
 		robot.mesure_distance(infos[0]);
 		robot.mesure_angle(infos[1]);
+		
+		//mise à jour du pwm envoyé aux moteurs pour l'asservissement
 		robot.asservir();
+		
+		//vérification des conditions de bloquage du robot
 		robot.gestion_blocage();
+		
+		//calcul de la nouvelle position courante du robot, en absolu sur la table (mm et radians)
 		robot.update_position();
 	}
 }
